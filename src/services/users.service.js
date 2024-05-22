1fe94c46a0bc70c918cc89a567249e8e44c409d0
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-05-22 23:36:22 +0300

    Add User Services initial setup

---
+++ src/services/users.service.js
@@ -0,0 +1,85 @@
+import { get, set, update, ref, query, orderByChild, equalTo } from 'firebase/database';
+import { db } from '../config/firebase-config.js';
+
+export const getUserByUsername = async (username) => {
+
+    return await get(ref(db, `users/${username}`));
+};
+
+export const createUserHandle = async (username, uid, email, phoneNumber, firstName, lastName, address, avatar) => {
+
+    return await set(ref(db, `users/${username}`), {
+        username,
+        uid,
+        email,
+        phoneNumber,
+        firstName,
+        lastName,
+        address,
+        avatar, // Check how to do it???
+        canBeInvited: false,
+        isAdmin: false,
+        isBanned: false,
+        createdO: Date.now()
+    })
+};
+
+export const getUserData = async (uid) => {
+
+    return await get(query(ref(db, 'users'), orderByChild('uid'), equalTo(uid)));
+};
+
+export const getAllUsersArray = async () => {
+    const userSnapshot = await get(ref(db, 'users'));
+    const users = [];
+
+    userSnapshot.forEach((user) => {
+        users.push(user.val());
+    });
+
+    return users;
+};
+
+export const updateUser = async (username, updatedData) => {
+
+    return await set(ref(db, `users/${username}`), { ...updatedData, updatedOn: Date.now()});
+};
+
+export const changeCanBeInvitedStatus = async (username, status) => {
+
+    return await set(ref(db, `users/${username}/canBeInvited`), status);
+};
+
+export const changeAdminStatus = async (username, status) => {
+
+    return set(ref(db, `users/${username}/isAdmin`), status)
+};
+
+export const changeBanStatus = async (username, status) => {
+
+    return await set(ref(db, `users/${username}/isBanned`), status);
+};
+
+//Check if there is another way to do it
+export const updateUserAvatar = async (username, avatarURL) => {
+    const changeAvatar = {};
+    changeAvatar[`users/${username}/avatarURL`] = avatarURL;
+
+    return await update(ref(db), changeAvatar);
+};
+
+//Test these 3 functions to see if they work as expected
+export const addUserCreatedEvent = async (username, eid) => {
+  
+    return await set(ref(db, `users/${username}/createdEvents/${eid}`));
+};
+
+export const addUserInvitedEvent = async (username, eid) => {
+  
+    return await set(ref(db, `users/${username}/invitedEvents/${eid}`));
+};
+
+export const addUserParticipatedEvent = async (username, eid) => {
+  
+    return await set(ref(db, `users/${username}/participatedEvents/${eid}`));
+};
