e61f1ef13d140c7ec26dd06edabb981b7c856120
Ivelin Banchev <ivelin.banchev@gmail.com>
2024-05-23 15:49:21 +0300

    Add Footer component

---
+++ src/components/Frame/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer className="footer footer-center p-4 flex flex-row justify-between bg-white">
+      <p>Team Project 3: SPA calendar for sports events</p>
+      <p>
+        Project Developed by: Bilyana Karcheva, Atanas Geov and Ivelin Banchev
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
