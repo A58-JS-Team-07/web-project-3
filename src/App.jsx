e59b64c32036dff0ec157905b6a2bb4642ea62c7
Ivelin Banchev <ivelin.banchev@gmail.com>
2024-05-23 17:43:01 +0300

    feat: Add Header and SideMenu sticky functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,16 @@ import Footer from "./components/Frame/Footer/Footer";
 
 function App() {
   return (
-    <div className="flex">
+    <div className="flex h-screen"> {/* Set the height of the flex container to full screen */}
       <div className="left w-1/6 bg-primary">
         <SideMenu />
       </div>
-      <div className="right w-5/6">
+      <div className="right w-5/6 flex flex-col"> {/* Make the right section a flex container */}
         <Header />
-        <Home />
-        <Footer />
+        <div className="overflow-auto flex-grow"> {/* Make the content area scrollable */}
+          <Home />
+          <Footer />
+        </div>
       </div>
     </div>
   );
