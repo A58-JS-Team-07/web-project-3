5b48219df1c8853693222fc51c719a1c9a6523df
Ivelin Banchev <ivelin.banchev@gmail.com>
2024-05-24 19:12:08 +0300

    feat: Add initial basic ver of Login/Register page

--- src/views/LoginRegister/LoginRegister.jsx
+++ src/views/LoginRegister/LoginRegister.jsx
@@ -1,10 +1,89 @@
+import Button from "../../components/Button/Button";
 
 function LoginRegister() {
   return (
-    <div>
-      <h1>Login or Register</h1>
-    </div>
-  )
+    <>
+      <div className="login">
+        <h1>Login</h1>
+        <div className="login__form">
+          <div className="login__form-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <div className="login__form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" />
+          </div>
+          <Button>Login</Button>
+        </div>
+      </div>
+
+      <div className="register">
+        <h1>Register</h1>
+        <div className="register__form">
+          <div className="register__row">
+            <div className="register__form-group">
+              <label htmlFor="firstName">First name</label>
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+              />
+            </div>
+            <div className="register__form-group">
+              <label htmlFor="lastName">Last name</label>
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+              />
+            </div>
+          </div>
+          <div className="register__row">
+            <div className="register__form-group">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+              />
+            </div>
+          </div>
+          <div className="register__row">
+            <div className="register__form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+              />
+            </div>
+          </div>
+          <div className="register__row">
+            <div className="register__form-group">
+              <label htmlFor="phone">Phone</label>
+              <input
+                type="phone"
+                id="phone"
+                name="phone"
+              />
+            </div>
+          </div>
+          <div className="register__row">
+            <div className="register__form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+              />
+            </div>
+          </div>
+          <Button>Register</Button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default LoginRegister;
