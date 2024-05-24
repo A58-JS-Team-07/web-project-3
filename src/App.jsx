44122a574befc713d88f8c0c0e5d174f390d207a
Ivelin Banchev <ivelin.banchev@gmail.com>
2024-05-24 14:44:11 +0300

    fix: Fix minor issues in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import "./App.css";
 import Home from "./views/Home/Home";
 import Header from "./components/Frame/Header/Header";
