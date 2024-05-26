14490371094c3256b365037ac8f55241d1e63f8d
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-05-26 22:58:23 +0300

    Add Loader component with spinner

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+
+const Loader = () => {
+
+    return (
+      <div className='h-screen flex justify-center  bg-gray-900' >
+        <span className="h-20 w-20 loading loading-spinner text-primary justify-center" style={{ position: 'absolute', height: '50%' }}></span>
+      </div>
+    );
+  };
+  
+  export default Loader;
