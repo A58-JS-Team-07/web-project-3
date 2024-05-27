e1f9c93de2048c41d05bd87fdca2d4f8d1d7e00c
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-05-27 13:12:46 +0300

    Replace throw new error with console.error

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -5,7 +5,7 @@ export const registerUser = (email, password) => {
     try {
         return createUserWithEmailAndPassword(auth, email, password);
     } catch (error) {
-        throw new Error('Error registering a user:' + error);
+        console.error('Error registering a user:' + error);
     }
 };
 
@@ -13,7 +13,7 @@ export const loginUser = (email, password) => {
     try {
         return signInWithEmailAndPassword(auth, email, password);
     } catch (error) {
-        throw new Error('Error signing in a user:' + error);
+        console.error('Error signing in a user:' + error);
     }
 };
 
@@ -21,6 +21,6 @@ export const logoutUser = () => {
     try {
         return signOut(auth);
     } catch (error) {
-        throw new Error('Error logging out:' + error);
+        console.error('Error logging out:' + error);
     }
 };
