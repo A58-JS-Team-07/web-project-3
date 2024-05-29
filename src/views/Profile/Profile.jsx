c434c0d4f62e3415a312266ed875a9939dd1f690
Ivelin Banchev <ivelin.banchev@gmail.com>
2024-05-29 12:37:34 +0300

    fix: Design minor corrections at Profile page

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -1,13 +1,13 @@
 import Button from "../../components/Button/Button";
 import { useContext, useState } from "react";
 import { AppContext } from "../../context/AppContext";
-import {   
+import {
   MIN_USERNAME_LENGTH,
   MAX_USERNAME_LENGTH,
   isValidEmail,
   isValidPhoneNumber,
   isValidPassword,
-  isValidName, 
+  isValidName,
 } from "../../common/constants";
 import { updateUser } from "../../services/users.service";
 import { toast } from "react-toastify";
@@ -26,12 +26,16 @@ function Profile() {
 
   const saveChanges = async () => {
     if (!isValidName(newUserData.firstName)) {
-      toast.error("First name must be between 1 and 30 characters and contain only letters!");
+      toast.error(
+        "First name must be between 1 and 30 characters and contain only letters!"
+      );
       return;
     }
 
     if (!isValidName(newUserData.lastName)) {
-      toast.error("Last name must be between 1 and 30 characters and contain only letters!");
+      toast.error(
+        "Last name must be between 1 and 30 characters and contain only letters!"
+      );
       return;
     }
 
@@ -44,8 +48,10 @@ function Profile() {
       toast.error("Please enter a valid email address!");
     }
 
-    if (newUserData.username.length < MIN_USERNAME_LENGTH ||
-      newUserData.username.length > MAX_USERNAME_LENGTH) {
+    if (
+      newUserData.username.length < MIN_USERNAME_LENGTH ||
+      newUserData.username.length > MAX_USERNAME_LENGTH
+    ) {
       toast.error("Username must be between 3 and 30 characters!");
       return;
     }
@@ -55,7 +61,6 @@ function Profile() {
     setAppState((prevState) => ({ ...prevState, userData: newUserData }));
   };
 
-
   const cancelChanges = () => {
     setIsEditing(false);
     setNewUserData(userData);
@@ -64,10 +69,10 @@ function Profile() {
   return (
     <div className="p-6">
       <h1 className="text-3xl font-bold mb-5">Profile</h1>
-      <div className="__container bg-base-200 flex flex-row w-full h-full p-20 gap-10 rounded-2xl">
-        <div className="inner__container bg-base-100 w-1/3 min-w-1/2 p-20 rounded-3xl  ">
+      <div className="__container bg-base-200 flex flex-row w-full h-full p-12 gap-10 rounded-2xl">
+        <div className="inner__container bg-base-100 w-1/3 min-w-1/2 p-12 rounded-3xl flex flex-col justify-between">
           <div className="avatar">
-            <div className="w-64 rounded-xl">
+            <div className="w-full rounded-xl">
               <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
             </div>
           </div>
@@ -75,87 +80,109 @@ function Profile() {
             <Button>Change Avatar</Button>
           </div>
         </div>
-        <div className="inner__container bg-base-100 w-2/3 min-w-1/2 p-20 rounded-3xl">
+        <div className="inner__container bg-base-100 w-2/3 min-w-1/2 p-12 rounded-3xl flex flex-col justify-between">
           {!isEditing ? (
             <>
-              <h1 className="text-2xl pb-3 font-semibold b-2">Profile information</h1>
-              <label className="label text-lg ">First name: {userData?.firstName}</label>
-              <label className="label text-lg ">Last name: {userData?.lastName}</label>
-              <label className="label text-lg ">Username: {userData?.username}</label>
-              <label className="label text-lg ">Email: {userData?.email}</label>
-              <label className="label text-lg ">Phone: {userData?.phoneNumber}</label>
-              {userData?.address && (
-                <label className="label text-lg ">Address: {userData?.address}</label>
-              )}
+              <div>
+                <h2 className="text-3xl pb-3 font-semibold mb-2">
+                  Profile information
+                </h2>
+                <div className="profile__info text-xl max-h-[500px] flex flex-col gap-1">
+                  <label className="label ">
+                    First name: {userData?.firstName}
+                  </label>
+                  <label className="label">
+                    Last name: {userData?.lastName}
+                  </label>
+                  <label className="label ">
+                    Username: {userData?.username}
+                  </label>
+                  <label className="label">Email: {userData?.email}</label>
+                  <label className="label ">
+                    Phone: {userData?.phoneNumber}
+                  </label>
+                  {userData?.address && (
+                    <label className="label">
+                      Address: {userData?.address}
+                    </label>
+                  )}
+                </div>
+              </div>
               <div className="form-upload-row flex gap-8 justify-between mt-5">
-                <Button onClick={() => setIsEditing(true)}>Update Profile</Button>
+                <Button onClick={() => setIsEditing(true)}>
+                  Update Profile
+                </Button>
               </div>
             </>
           ) : (
             <>
-              <div className="flex flex-col gap-2 p-3">
-                <input
-                  className="input input-bordered"
-                  value={newUserData.firstName}
-                  onChange={updateForm("firstName")}
-                  placeholder="First Name"
-                  type="text"
-                  id="firstName"
-                  name="firstName"
-                />
-              </div>
-              <div className="flex flex-col gap-2 p-3">
-                <input
-                  className="input input-bordered"
-                  value={newUserData.lastName}
-                  onChange={updateForm("lastName")}
-                  placeholder="Last Name"
-                  type="text"
-                  id="lastName"
-                  name="lastName"
-                />
-              </div>
-              <div className="flex flex-col gap-2 p-3">
-                <input
-                  className="input input-bordered"
-                  value={newUserData.email}
-                  onChange={updateForm("email")}
-                  placeholder="Email"
-                  type="text"
-                  id="email"
-                  name="email"
-                />
-              </div>
-              <div className="flex flex-col gap-2 p-3">
-                <input
-                  className="input input-bordered"
-                  value={newUserData.phoneNumber}
-                  onChange={updateForm("phoneNumber")}
-                  placeholder="Phone number"
-                  type="text"
-                  id="phoneNumber"
-                  name="phoneNumber"
-                />
-              </div>
-              <div className="flex flex-col gap-2 p-3">
-                <input
-                  className="input input-bordered"
-                  value={newUserData.address}
-                  onChange={updateForm("address")}
-                  placeholder="City, Country"
-                  type="text"
-                  id="address"
-                  name="address"
-                />
+              <div>
+                <h2 className="text-3xl pb-3 font-semibold mb-2 ">
+                  Edit profile information
+                </h2>
+                <div className="profile__edit flex flex-col gap-4">
+                  <div className="flex flex-col gap-2 ">
+                    <input
+                      className="input input-bordered"
+                      value={newUserData.firstName}
+                      onChange={updateForm("firstName")}
+                      placeholder="First Name"
+                      type="text"
+                      id="firstName"
+                      name="firstName"
+                    />
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <input
+                      className="input input-bordered"
+                      value={newUserData.lastName}
+                      onChange={updateForm("lastName")}
+                      placeholder="Last Name"
+                      type="text"
+                      id="lastName"
+                      name="lastName"
+                    />
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <input
+                      className="input input-bordered"
+                      value={newUserData.email}
+                      onChange={updateForm("email")}
+                      placeholder="Email"
+                      type="text"
+                      id="email"
+                      name="email"
+                    />
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <input
+                      className="input input-bordered"
+                      value={newUserData.phoneNumber}
+                      onChange={updateForm("phoneNumber")}
+                      placeholder="Phone number"
+                      type="text"
+                      id="phoneNumber"
+                      name="phoneNumber"
+                    />
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <input
+                      className="input input-bordered"
+                      value={newUserData.address}
+                      onChange={updateForm("address")}
+                      placeholder="City, Country"
+                      type="text"
+                      id="address"
+                      name="address"
+                    />
+                  </div>
+                </div>
               </div>
               <div className="form-update-row flex gap-8 justify-between mt-5">
-                <Button
-                onClick={() => saveChanges()}
-                >Save Changes</Button>
-                <button
-                className="btn text-lg"
-                onClick={() => cancelChanges()}
-                >Cancel</button>
+                <Button onClick={() => saveChanges()}>Save Changes</Button>
+                <button className="btn text-lg" onClick={() => cancelChanges()}>
+                  Cancel
+                </button>
               </div>
             </>
           )}
@@ -167,23 +194,23 @@ function Profile() {
   // (
   //   <div>
   //     <h2 className="text-3xl font-bold mb-5">Profile</h2>
-      // <div className="is-private flex flex-col gap-2 w-4/12 p-3 bg-base-100 rounded-md">
-      //   <div className="private-checkbox flex gap-4 items-center">
-      //     <span className="label-text text-lg font-semibold">
-      //       Allow others to invite me to events
-      //     </span>
+  // <div className="is-private flex flex-col gap-2 w-4/12 p-3 bg-base-100 rounded-md">
+  //   <div className="private-checkbox flex gap-4 items-center">
+  //     <span className="label-text text-lg font-semibold">
+  //       Allow others to invite me to events
+  //     </span>
 
-      //     <input
-      //       type="checkbox"
-      //       className="toggle"
-      //       name="private-event"
-      //       id="private-event"
-      //     />
-      //   </div>
-      //   <span className="label-text-alt">
-      //     * You have the option to opt-out of events invitations.
-      //   </span>
-      // </div>
+  //     <input
+  //       type="checkbox"
+  //       className="toggle"
+  //       name="private-event"
+  //       id="private-event"
+  //     />
+  //   </div>
+  //   <span className="label-text-alt">
+  //     * You have the option to opt-out of events invitations.
+  //   </span>
+  // </div>
   //   </div>
   // )
 }
