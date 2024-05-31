e90e322a8a9cfed0a2e845152ddcc7b6a73b0759
Ivelin Banchev <ivelin.banchev@gmail.com>
2024-05-31 15:17:19 +0300

    Add additinal getUser hook to firebase that return snapshot

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,114 +1,154 @@
-import { get, set, update, ref, query, orderByChild, equalTo } from 'firebase/database';
-import { db } from '../config/firebase-config.js';
+import {
+  get,
+  set,
+  update,
+  ref,
+  query,
+  orderByChild,
+  equalTo,
+} from "firebase/database";
+import { db } from "../config/firebase-config.js";
 
 export const getUserByUsername = async (username) => {
-    try {
-        return await get(ref(db, `users/${username}`));
-    } catch (error) {
-        console.error('Error getting user data by username: ' + error)
-        throw error;
-    }
+  try {
+    return await get(ref(db, `users/${username}`));
+  } catch (error) {
+    console.error("Error getting user data by username: " + error);
+  }
 };
 
-export const createUser = async (username, uid, email, phoneNumber, firstName, lastName) => {
-    try {
-        return await set(ref(db, `users/${username}`), {
-            username,
-            uid,
-            email,
-            phoneNumber,
-            firstName,
-            lastName,
-            canBeInvited: true,
-            isAdmin: false,
-            isBanned: false,
-            createdOn: Date.now()
-        });
-    } catch (error) {
-        console.error('Error creating user: ' + error);
+export const getUserByUsernameSnapshot = async (username) => {
+  try {
+    const userRef = ref(db, `users/${username}`);
+    const userSnapshot = await get(userRef);
+
+    if (userSnapshot.exists()) {
+      return userSnapshot.val();
+    } else {
+      console.error(
+        "Error in users.services > getUserSnapshot > No such user found"
+      );
     }
+  } catch (error) {
+    console.error(
+      "Error in users.services > getUserByUsernameSnapshot:",
+      error
+    );
+    throw error;
+  }
+};
+
+export const createUser = async (
+  username,
+  uid,
+  email,
+  phoneNumber,
+  firstName,
+  lastName
+) => {
+  try {
+    return await set(ref(db, `users/${username}`), {
+      username,
+      uid,
+      email,
+      phoneNumber,
+      firstName,
+      lastName,
+      canBeInvited: true,
+      isAdmin: false,
+      isBanned: false,
+      createdOn: Date.now(),
+    });
+  } catch (error) {
+    console.error("Error creating user: " + error);
+  }
 };
 
 export const getUserData = async (uid) => {
-    try {
-        return await get(query(ref(db, 'users'), orderByChild('uid'), equalTo(uid)));
-    } catch (error) {
-        console.error('Error getting user data: ' + error);
-    }
+  try {
+    return await get(
+      query(ref(db, "users"), orderByChild("uid"), equalTo(uid))
+    );
+  } catch (error) {
+    console.error("Error getting user data: " + error);
+  }
 };
 
 export const getAllUsersArray = async () => {
-    try {
-        const usersSnapshot = await get(ref(db, 'users'));
-        return Object.values(usersSnapshot.val());
-    } catch (error) {
-        console.error('Error getting all users: ' + error);
-    }
+  try {
+    const usersSnapshot = await get(ref(db, "users"));
+    return Object.values(usersSnapshot.val());
+  } catch (error) {
+    console.error("Error getting all users: " + error);
+  }
 };
 
 export const updateUser = async (username, updatedData) => {
-    try {
-        await set(ref(db, `users/${username}`), { ...updatedData, updatedOn: Date.now()});
-    } catch (error) {
-        console.error('Error updating user:' + error);
-    }
+  try {
+    await set(ref(db, `users/${username}`), {
+      ...updatedData,
+      updatedOn: Date.now(),
+    });
+  } catch (error) {
+    console.error("Error updating user:" + error);
+  }
 };
 
 export const changeCanBeInvitedStatus = async (username, status) => {
-    try {
-        await set(ref(db, `users/${username}/canBeInvited`), status);
-    } catch (error) {
-        console.error('Error changing can be invited status:' + error);
-    }
+  try {
+    await set(ref(db, `users/${username}/canBeInvited`), status);
+  } catch (error) {
+    console.error("Error changing can be invited status:" + error);
+  }
 };
 
 export const changeAdminStatus = async (username, status) => {
-    try {
-        await set(ref(db, `users/${username}/isAdmin`), status);
-    } catch (error) {
-        console.error('Error changing admin status:' + error);
-    }
+  try {
+    await set(ref(db, `users/${username}/isAdmin`), status);
+  } catch (error) {
+    console.error("Error changing admin status:" + error);
+  }
 };
 
 export const changeBanStatus = async (username, status) => {
-    try {
-        await set(ref(db, `users/${username}/isBanned`), status);
-    } catch (error) {
-        console.error('Error changing ban status:' + error);
-    }
+  try {
+    await set(ref(db, `users/${username}/isBanned`), status);
+  } catch (error) {
+    console.error("Error changing ban status:" + error);
+  }
 };
 
 //Check if there is another way to do it
 export const updateUserAvatar = async (username, avatarURL) => {
-    try {
-        const userRef = ref(db, `users/${username}`);
-        await update(userRef, { avatar: avatarURL});
-    } catch (error) {
-        console.error('Error updating avatar' + error);
-    }
+  try {
+    const userRef = ref(db, `users/${username}`);
+    await update(userRef, { avatar: avatarURL });
+  } catch (error) {
+    console.error("Error updating avatar" + error);
+  }
 };
 
 //Transfer these 3 functions to event services and test them to see if they work as expected
 export const addUserCreatedEvent = async (username, eid) => {
-    try {
-        await set(ref(db, `users/${username}/createdEvents/${eid}`));
-    } catch (error) {
-        console.error('Error adding created event to user:' + error);
-    }
+  try {
+    await set(ref(db, `users/${username}/createdEvents/${eid}`));
+  } catch (error) {
+    console.error("Error adding created event to user:" + error);
+  }
 };
 
 export const addUserInvitedEvent = async (username, eid) => {
-    try {
-        await set(ref(db, `users/${username}/invitedEvents/${eid}`));
-    } catch (error) {
-        console.error('Error adding invited event to user:')
-    }
+  try {
+    await set(ref(db, `users/${username}/invitedEvents/${eid}`));
+  } catch (error) {
+    console.error("Error adding invited event to user:");
+  }
 };
 
 export const addUserParticipatedEvent = async (username, eid) => {
-    try {
-        await set(ref(db, `users/${username}/participatedEvents/${eid}`));
-    } catch (error) {
-        console.error('Error adding participated event to user:' + error);
-    }
+  try {
+    await set(ref(db, `users/${username}/participatedEvents/${eid}`));
+  } catch (error) {
+    console.error("Error adding participated event to user:" + error);
+  }
 };
