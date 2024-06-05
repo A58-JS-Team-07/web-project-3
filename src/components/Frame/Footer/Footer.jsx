27854843f1c207188e36b5f8018bcd3938a15749
Ivelin Banchev <ivelin.banchev@gmail.com>
2024-06-05 16:50:57 +0300

    fix: Footer design correction

--- src/components/Frame/Footer/Footer.jsx
+++ src/components/Frame/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="footer footer-center p-4 flex flex-row justify-between bg-white">
+    <footer className="footer footer-center p-4 flex flex-row justify-between bg-base-200">
       <p>Team Project 3: SPA calendar for sports events</p>
       <p>
         Project Developed by: Bilyana Karcheva, Atanas Geov and Ivelin Banchev
