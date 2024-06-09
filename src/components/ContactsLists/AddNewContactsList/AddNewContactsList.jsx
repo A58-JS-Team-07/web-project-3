0dc1813a64cc3f071a9987de485e5b5a39eb9284
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-06-09 17:39:52 +0300

    fix: Amend style of add contacts list form

--- src/components/ContactsLists/AddNewContactsList/AddNewContactsList.jsx
+++ src/components/ContactsLists/AddNewContactsList/AddNewContactsList.jsx
@@ -53,7 +53,7 @@ function AddNewContactsListForm({ showModal, setShowModal = () => { } }) {
     <div>
       {showModal ? (
         <div className="create-event-modal-backdrop absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  min-w-full min-h-full bg-black z-10 bg-opacity-60 ">
-          <div className="create-event-modal m-8 rounded-bl-2xl rounded-tl-2xl bg-base-200 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 min-w-[30%] min-h-[30%] max-h-[80%] overflow-auto max-w-7xl z-10 px-8 py-7">
+          <div className="create-event-modal m-8 rounded-2xl rounded-2xl bg-base-200 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 min-w-[30%] min-h-[30%] max-h-[80%] overflow-auto max-w-7xl z-10 px-8 py-7">
             <h2 className="text-3xl font-bold mb-5">Create List</h2>
             <button
               className="btn rounded-full p-0 px-2 text-3xl absolute right-5 top-6"
@@ -61,13 +61,14 @@ function AddNewContactsListForm({ showModal, setShowModal = () => { } }) {
             >
               <IoClose />
             </button>
-
-            <input
+            <label className="input input-bordered flex mb-8 grow items-center gap-2 max-w-[400px]">
+            <input className="grow"
               type="text"
               value={newContactsListName}
               onChange={handleChange}
               placeholder="Enter new contacts list name"
             />
+            </label>
             <div className="form-update-row flex gap-8 justify-between mt-5">
               <Button onClick={handleSubmit}>Create list</Button>
             </div>
