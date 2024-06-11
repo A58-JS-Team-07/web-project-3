ee0ff724f6e869844916efab7b0f1bc48c435837
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-06-11 15:30:44 +0300

    feat: Add Load more button to users list in Admin page

--- src/views/AdminCenter/AdminCenter.jsx
+++ src/views/AdminCenter/AdminCenter.jsx
@@ -5,12 +5,15 @@ import { changeBanStatus } from "../../services/users.service";
 import UserSnippet from "../../components/UserSnippet/UserSnippet";
 import { toast } from "react-toastify";
 import { searchUsers, getAllUsersExcludeCurrent } from "../../services/users.service";
+import Button from "../../components/Button/Button";
 
 function AdminCenter() {
   const [users, setUsers] = useState([]);
   const { userData } = useContext(AppContext);
   const [searchParams, setSearchParams] = useSearchParams();
   const search = searchParams.get("search") || "";
+  const [displayCount, setDisplayCount] = useState(7);
+  const [userCount, setUserCount] = useState(0);
 
   const setSearch = (value) => {
     setSearchParams({ search: value });
@@ -31,9 +34,21 @@ function AdminCenter() {
         return res;
       });
       setUsers(loadUsers);
+      setUserCount(loadUsers.length);
     }
     loadAllUsersList();
   }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.innerHeight + document.documentElement.scrollTop < document.documentElement.offsetHeight - 50) return;
+      setDisplayCount(prevCount => prevCount + 7);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [displayCount]);
 
   function handleBanUser(user) {
     const currStatus = user.isBanned;
@@ -82,18 +97,32 @@ function AdminCenter() {
       <div className="user-list bg-base-200 w-4/5 min-w-1/2 p-4 ml-5 rounded-3xl">
         {users
           .filter((user) => user.uid !== userData.uid)
+          .slice(0, displayCount)
           .map((user) => {
             return (
-              <div className="user-snippet-data flex flex-row gap-4 bg-base-100 m-2 p-3 rounded-xl">
-                <UserSnippet
-                  key={user.uid}
-                  user={user}
-                  handleBanUser={handleBanUser}
-                  adminActions={true}
-                />
-              </div>
+              <>
+                <div className="user-snippet-data flex flex-row gap-4 bg-base-100 m-2 p-3 rounded-xl">
+                  <UserSnippet
+                    key={user.uid}
+                    user={user}
+                    handleBanUser={handleBanUser}
+                    adminActions={true}
+                  />
+                </div>
+              </>
             );
           })}
+        {/* {users.length === 0 && <p>No users found</p>} */}
+        {users.length > displayCount && (
+          <div className="flex justify-center p-4">
+            <Button
+              style="load-more-btn btn-active btn-ghost"
+              onClick={() => setDisplayCount((prevCount) => prevCount + 7)}
+            >
+              Load more
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   );
