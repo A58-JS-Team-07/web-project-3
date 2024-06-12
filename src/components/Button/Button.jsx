5c48c1e519034561b382b11410f5213ff5918ffe
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-06-12 18:02:35 +0300

    doc: Button.jsx amend PropTypes and add JSDoc

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,4 +1,12 @@
-import propTypes from "prop-types";
+import PropTypes from "prop-types";
+
+/**
+ * Button component renders a styled button with customizable content and behavior.
+ * @param {string} props.children - The content of the button
+ * @param {function} props.onClick - The function to be executed when the button is clicked
+ * @param {string} props.style - The style of the button
+ * @returns {JSX.Element}
+ */
 
 function Button({ children, onClick = () => {}, style = "btn-secondary text-white" }) {
   return (
@@ -9,9 +17,9 @@ function Button({ children, onClick = () => {}, style = "btn-secondary text-whit
 }
 
 Button.propTypes = {
-  children: propTypes.any.isRequired,
-  onClick: propTypes.func,
-  style: propTypes.string,
+  children: PropTypes.any.isRequired,
+  onClick: PropTypes.func,
+  style: PropTypes.string,
 }
 
 export default Button;
