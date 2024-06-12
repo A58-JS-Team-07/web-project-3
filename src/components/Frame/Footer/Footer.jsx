611e2f64c7b21753e0a0d2bbd22de47c9128e217
Ivelin Banchev <ivelin.banchev@gmail.com>
2024-06-12 10:42:53 +0300

    fix: Footer developers link to github add, Nasko removed

--- src/components/Frame/Footer/Footer.jsx
+++ src/components/Frame/Footer/Footer.jsx
@@ -2,9 +2,16 @@ function Footer() {
   return (
     <footer className="footer footer-center p-4 flex flex-row justify-between bg-base-200">
       <p>Team Project 3: SPA calendar for sports events</p>
-      <p>
-        Project Developed by: Bilyana Karcheva, Atanas Geov and Ivelin Banchev
-      </p>
+      <div className="flex flex-row">
+        Project Developed by:
+        <a href="https://github.com/bilyanakarcheva" target="_blank">
+          Bilyana Karcheva
+        </a>
+        and
+        <a href="https://github.com/banch3v" target="_blank">
+          Ivelin Banchev
+        </a>
+      </div>
     </footer>
   );
 }
