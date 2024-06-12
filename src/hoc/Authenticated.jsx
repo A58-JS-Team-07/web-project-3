4dfd9343971adcc99598994b63d1492417b81a4d
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-06-12 19:05:15 +0300

    doc: Add JSDoc

--- src/hoc/Authenticated.jsx
+++ src/hoc/Authenticated.jsx
@@ -3,6 +3,12 @@ import { useContext } from "react";
 import { Navigate, useLocation } from 'react-router-dom';
 import { AppContext } from "../context/AppContext";
 
+/**
+ * This component protects the routes from unauthorized access.
+ * @param {{children: any }} props - The children to be rendered
+ * @returns {JSX.Element}
+ */
+
 export default function Authenticated ({ children }) {
     const { user } = useContext(AppContext);
     const location = useLocation();
