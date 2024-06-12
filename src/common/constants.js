3e9bbc16f9bebdda52d97129c61818a3161e15e2
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-06-12 10:14:52 +0300

    fix: Set the Weather API key to take the value from the .env file which is not commited

--- src/common/constants.js
+++ src/common/constants.js
@@ -119,4 +119,4 @@ export const MIN_CONTACT_LIST_NAME_LENGTH = 3;
 
 export const MAX_CONTACT_LIST_NAME_LENGTH = 30;
 
-export const WEATHER_API_KEY = 'bfcc2d0b5918468dbe8132855241106';
+export const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
