d8b404c29770f313b28f8e59abbf78f0ab9d14c2
Ivelin Banchev <ivelin.banchev@gmail.com>
2024-06-12 18:24:17 +0300

    doc: auth.service add JSDoc

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,26 +1,50 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import { auth } from "../config/firebase-config.js";
 
+/**
+ * Registers a new user with the provided email and password to the Firebase authentication.
+ *
+ * @param {string} email - The email of the user.
+ * @param {string} password - The password of the user.
+ */
+
 export const registerUser = (email, password) => {
-    try {
-        return createUserWithEmailAndPassword(auth, email, password);
-    } catch (error) {
-        console.error('Error registering a user:' + error);
-    }
+  try {
+    return createUserWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.error("Error registering a user:" + error);
+  }
 };
+
+/**
+ * Logs in a user with the provided email and password to the Firebase authentication.
+ *
+ * @param {string} email - The email of the user.
+ * @param {string} password - The password of the user.
+ */
 
 export const loginUser = (email, password) => {
-    try {
-        return signInWithEmailAndPassword(auth, email, password);
-    } catch (error) {
-        console.error('Error signing in a user:' + error);
-    }
+  try {
+    return signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.error("Error signing in a user:" + error);
+  }
 };
 
+/**
+ * Logs out the current user from the Firebase authentication.
+ *
+ * @returns {Promise<void>} - A promise that resolves when the user is logged out.
+ */
+
 export const logoutUser = () => {
-    try {
-        return signOut(auth);
-    } catch (error) {
-        console.error('Error logging out:' + error);
-    }
+  try {
+    return signOut(auth);
+  } catch (error) {
+    console.error("Error logging out:" + error);
+  }
 };
